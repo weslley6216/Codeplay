ee99de52d38338d6dba4eb1b260a9a5f7669b22a
Weslley Campos <weslley.camphos@hotmail.com>
2021-05-17 19:49:22 -0300

    Validaçoes

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,4 @@
 class Instructor < ApplicationRecord
-  
+  validates :name, :email, presence: { message: 'não pode ficar em branco' }
+  validates :email, uniqueness: { message: 'O email informado já está em uso' }
 end
