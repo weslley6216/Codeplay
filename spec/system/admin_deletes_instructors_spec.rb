5c10ece6f7d815cda27166289f0bb7deb0cc72ca
Weslley Campos <weslley.camphos@hotmail.com>
2021-05-19 10:01:41 -0300

    Apagando Professores

--- spec/system/admin_deletes_instructors_spec.rb
+++ spec/system/admin_deletes_instructors_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe 'admin deletes instructors' do
   it 'successfully' do
-    Instructor.create!(name: 'Gustavo Guanabara', email: 'guanabara@codeplay.com',
-                       bio: 'Professor por vocação')
+    instructor = Instructor.create!(name: 'Gustavo Guanabara', email: 'guanabara@codeplay.com',
+                                    bio: 'Professor por vocação')
+    instructor.profile_picture.attach(io: File.open('spec/fixtures/foto_perfil.jpeg'),
+                                      filename: 'foto_perfil.jpeg')
     visit root_path
     click_on 'Professores'
     click_on 'Gustavo Guanabara'
-    click_on 'Apagar'
-    
-    expect(current_path).to eq(instructors_path)
+   
+    expect { click_link 'Apagar' }.to change { Instructor.count }.by(-1)
     expect(page).to have_content('Professor removido com sucesso!')
-    expect(Instructor.count).to eq(0)
+    expect(current_path).to eq(instructors_path)
+      
   end
 end
