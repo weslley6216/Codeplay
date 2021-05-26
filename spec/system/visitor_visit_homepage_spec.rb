f1b060f3baf34aba47e6e1a638833852bba211a3
Weslley Campos <weslley.camphos@hotmail.com>
2021-05-25 21:31:33 -0300

    Adcionando bootstrap na aplicaçao

--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -4,8 +4,8 @@ describe 'Visitor visit homepage' do
   it 'successfully' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Codeplay')
-    expect(page).to have_css('h3', text: 'Boas vindas ao sistema de gestão de '\
+    expect(page).to have_css('h1', text: 'CodePlay')
+    expect(page).to have_css('p', text: 'Boas vindas ao sistema de gestão de '\
                                          'cursos e aulas')
   end
 end
