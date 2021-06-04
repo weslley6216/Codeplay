6a225bef5d77c3e1f961599980756cc6d0449074
Weslley Campos <weslley.camphos@hotmail.com>
2021-06-04 01:01:08 -0300

    Refatorando testes e views do visitante

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,64 +1,14 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: %i[edit update show destroy enroll]
-  #before_action :authenticate_user!, except: %i[index]
-  
-  def index
-    @courses = Course.all
-  end
+  before_action :set_course, only: %i[show]
 
   def show
   end
 
-  def new
-    @instructors = Instructor.all
-    @course = Course.new
-  end
-
-  def edit
-    @instructors = Instructor.all
-  end
-
-  def create
-    @course = Course.new(course_params)
-    if @course.save
-      redirect_to [:admin, @course], notice: t('messages.course_created')
-    else
-      @instructors = Instructor.all
-      render :new
-    end
-  end
-
-  def update
-    if @course.update(course_params)
-      redirect_to [:admin, @course], notice: t('messages.course_updated')
-    else
-      @instructors = Instructor.all
-      render :edit
-    end
-  end
-
-  def destroy
-    @course.destroy
-    redirect_to admin_courses_path, notice: 'Curso removido com sucesso'
-  end
-
-  def enroll
-    current_user.enrollments.create(course: @course, price: @course.price)
-    redirect_to my_enrolls_courses_path, notice: 'Curso comprado com sucesso'
-  end
-
-  def my_enrolls
-    @enrollments = current_user.enrollments
-  end
 
   private
 
   def set_course
     @course = Course.find(params[:id])
-  end
-
-  def course_params
-    params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline, :instructor_id)
   end
 
 end
--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -7,5 +7,29 @@ describe 'Visitor visit homepage' do
     expect(page).to have_css('h1', text: 'CodePlay')
     expect(page).to have_css('p', text: 'Boas vindas ao sistema de gestão de '\
                                          'cursos e aulas')
+    expect(page).to have_content('Cadastre-se')
+    expect(page).to have_content('Entrar')
+    expect(page).to have_content('Cursos Disponíveis')
+  end
+
+  it 'and click on an available course' do
+    instructor = Instructor.create!(name: 'Gustavo Guanabara',
+                                    email: 'guanabara@codeplay.com')
+    course = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
+                            code: 'RUBYBASIC', price: 10,
+                            enrollment_deadline: '22/12/2033', instructor: instructor)
+
+    login_user
+    visit course_path(course)
+
+    expect(page).to have_content('Ruby')
+    expect(page).to have_content('Um curso de Ruby')
+    expect(page).to have_content('RUBYBASIC')
+    expect(page).to have_content('R$ 10,00')
+    expect(page).to have_content('22/12/2033')
+    expect(page).to have_content('Gustavo Guanabara')
+    expect(page).to have_text('Faça login para comprar este curso')
+    expect(page).to have_link('Voltar', href: root_path)
+
   end
 end
